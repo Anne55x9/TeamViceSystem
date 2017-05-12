654225d58df7ee406eb79f6a6a797a2c3a002545
Anne Sofie Molsted Wanscher <anne55x9@edu.easj.dk>
2017-05-12 12:47:48 +0200

    Tilføjet Singleton instance attributten som er static og indeholder conditional statement som sikre kun en instance er kaldt i systemet.

---
+++ TeamViceSystem/Model/AppartAssignOverviewSingleton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamViceSystem.Model
+{
+    public class AppartAssignOverviewSingleton
+    {
+        private static AppartAssignOverviewSingleton instance;
+
+        public static AppartAssignOverviewSingleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new AppartAssignOverviewSingleton();
+                }
+                return instance; 
+            }
+        }
+
+
+    }
+}
