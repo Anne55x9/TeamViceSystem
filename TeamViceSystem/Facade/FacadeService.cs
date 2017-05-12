ccbc3e7e86ff4882961ebadcc38dc6ae5e46e154
Anne Sofie Molsted Wanscher <anne55x9@edu.easj.dk>
2017-05-12 13:50:51 +0200

    Oprettet Http kald Get til ApparmentAssignmentoverview listen fra tilhørende Singletonklasse med AppartmentAssignment attributter.

---
+++ TeamViceSystem/Facade/FacadeService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using TeamViceSystem.Model;
+
+namespace TeamViceSystem.Facade
+{
+    public class FacadeService
+    {
+
+        private const string serverUrl = "http://01vicewebservice20170511065111.azurewebsites.net";
+
+        public static async Task<ObservableCollection<AppartmentAssignment>> GetAppartAssignOverview()
+        {
+            ObservableCollection<AppartmentAssignment> tempList = new ObservableCollection<AppartmentAssignment>();
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Clear();
+                client.BaseAddress = new Uri(serverUrl);
+
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync("api/Appartments");
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        tempList = await response.Content.ReadAsAsync<ObservableCollection<AppartmentAssignment>>();
+                    }
+                }
+                catch (Exception e)
+                {
+                    System.Diagnostics.Debug.Write($"Exception: {e}");
+                    tempList = null;
+
+                }
+
+                return tempList;
+            }
+           
+        }
+
+    }
+}
--- TeamViceSystem/ViewModel/AppartAssignViewModel.cs
+++ TeamViceSystem/ViewModel/AppartAssignViewModel.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using TeamViceSystem.Model;
 
+
 namespace TeamViceSystem.ViewModel
 {
     public class AppartAssignViewModel: INotifyPropertyChanged
