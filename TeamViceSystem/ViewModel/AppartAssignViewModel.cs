25c80d40de77a139fb1a3fb2e1b3aea9b89d22af
Anne Sofie Molsted Wanscher <anne55x9@edu.easj.dk>
2017-05-12 12:58:22 +0200

    AppartAssignViewModel implementerer InotifyPropertyChanged Interfaset.

---
+++ TeamViceSystem/ViewModel/AppartAssignViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamViceSystem.ViewModel
+{
+    public class AppartAssignViewModel: INotifyPropertyChanged
+    {
+
+
+        #region INotifyPropertyChanged Interfaset
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyname)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyname));
+            }
+        }
+
+        #endregion
+
+
+    }
+}
